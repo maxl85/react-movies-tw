0ee7577d1edda1f117064c391a7eb8b379e81664
maxl85 <maxl85@mail.ru>
2023-05-05 01:07:56 +0300

    layout

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const { Content } = Layout;
+
+const Main = () => {
+  return (
+    <Content className='flex items-center justify-center'>
+      Content
+    </Content>
+  )
+}
+
+export default Main
