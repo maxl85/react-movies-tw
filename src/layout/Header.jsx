0bc74b0c94f74b945d9e76bcd02a0879150e608c
maxl85 <maxl85@mail.ru>
2023-05-05 23:29:23 +0300

    Responsive Design

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -8,17 +8,17 @@ const { Header } = Layout;
 const _Header = () => {
   return (
     <>
-    <Header>
-      <div className='flex justify-between h-full'>
-        <div className='flex items-center gap-1'>
-          <div className='text-white text-2xl'>Movies</div>
-          <BiCameraMovie className='text-white text-3xl' />
+      <Header>
+        <div className='flex justify-between h-full'>
+          <div className='flex items-center gap-1'>
+            <div className='text-white text-2xl'>Movies</div>
+            <BiCameraMovie className='text-white text-3xl' />
+          </div>
+          <a target='_blank' rel="noopener noreferrer" href='https://github.com/maxl85/react-movies-tw'>
+            <AiOutlineGithub className='text-white text-3xl h-full hover:text-red-200 hover:cursor-pointer' />
+          </a>
         </div>
-        <AiOutlineGithub className='text-white text-3xl h-full' />
-      </div>
-    </Header>
-    
-    
+      </Header>
     </>
   )
 }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,64 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Col, Row } from 'antd';
 
 const { Content } = Layout;
 
 const Main = () => {
   return (
-    <Content className='flex items-center justify-center'>
+    <>
+      {/* <Content className='flex items-center justify-center'>
       Content
-    </Content>
+    </Content> */}
+      <Content>
+        <Row gutter={[16, 16]}>
+          <Col span={6}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col span={6}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col span={6}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col span={6}>
+            <div className='box1'>col</div>
+          </Col>
+          
+          <Col span={6}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col span={6}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col span={6}>
+            <div className='box1'>col</div>
+          </Col>
+        </Row>
+      </Content>
+      
+      {/* <Content>
+        <Row gutter={[16, 16]}>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
+            <div className='box1'>col</div>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={6} xl={4}>
+            <div className='box1'>col</div>
+          </Col>
+        </Row>
+      </Content> */}
+    </>
   )
 }
 
